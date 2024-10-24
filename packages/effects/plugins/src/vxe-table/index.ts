d20543eb462baeba70fd674f0de839cae994ac77
QJia <532612154@qq.com>
2024-10-24 14:59:21 +0800

    chore: 从vxe-table导出VxeGridDefines

--- packages/effects/plugins/src/vxe-table/index.ts
+++ packages/effects/plugins/src/vxe-table/index.ts
@@ -1,4 +1,4 @@
 export { setupVbenVxeTable } from './init';
 export * from './use-vxe-grid';
 export { default as VbenVxeGrid } from './use-vxe-grid.vue';
-export type { VxeGridListeners, VxeGridProps } from 'vxe-table';
+export type { VxeGridDefines, VxeGridListeners, VxeGridProps } from 'vxe-table';
