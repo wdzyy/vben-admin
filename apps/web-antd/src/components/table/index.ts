551f832f94690a99ff70ac3916c108ef2c0c92f3
QJia <532612154@qq.com>
2025-04-02 14:39:32 +0800

    feat: 新增OptionsTag组件

--- apps/web-antd/src/components/table/index.ts
+++ apps/web-antd/src/components/table/index.ts
@@ -1,2 +1,3 @@
+export { default as OptionsTag } from './src/options-tag.vue';
 export { default as TablePullDown } from './src/table-pull-down.vue';
 export { default as TableSwitch } from './src/table-switch.vue';
